8ee2cd657424656981fddc326b070a3fc144b028
Rohit.Chand <rohit.chand@cognizant.com>
2020-06-09 08:09:42 +0530

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
+		//this is test comment
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
 
